ee85a804a49d719a52ea8d5761e2f3d343d1c23f
NIKITA LOGINOV <80138722+morenekitov@users.noreply.github.com>
2023-04-13 22:57:26 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,13 +6,19 @@ import scipy.stats as st
 chat_id = 287133833 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    x = x[10:-10]
-    y = y[10:-10]
+    x = x[:-5]
+    y = y[:-5]
     
     a = 0.05
     #H0 - средние равны, x_mean = y_mean
-    z_obs, p_val_mean = ztest (y,x,0,usevar='pooled',alternative='smaller')
-    
+    _, p_krusk = st.kruskal(y,x)
 
 
-    return (p_val_mean < a)
+
+    if p_krusk<a:
+        print('Крускалл: ', p_krusk)
+        t_val, p_stud = st.ttest_ind(y,x,alternative='less',equal_var=False)
+        print(t_val, p_stud)
+        return p_stud < a
+    else:
+        return False
