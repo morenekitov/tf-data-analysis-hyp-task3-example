fcea4b4cf4d5e5efdaf229ea6f8ad3d477e10477
NIKITA LOGINOV <80138722+morenekitov@users.noreply.github.com>
2023-04-13 19:31:42 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from statsmodels.stats.weightstats import ztest
+import scipy.stats as st
 
 chat_id = 287133833 # Ваш chat ID, не меняйте название переменной
 
