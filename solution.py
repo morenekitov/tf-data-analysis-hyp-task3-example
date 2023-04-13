1737e896d1348072a68425aa5e5f2dfa76f72993
NIKITA LOGINOV <80138722+morenekitov@users.noreply.github.com>
2023-04-13 22:08:10 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,7 +6,13 @@ import scipy.stats as st
 chat_id = 287133833 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    a = 0.05
+    x = x[10:-10]
+    y = y[10:-10]
     
+    a = 0.05
+    #H0 - средние равны, x_mean = y_mean
     z_obs, p_val_mean = ztest (y,x,0,usevar='pooled',alternative='smaller')
+    
+
+
     return (p_val_mean < a)
